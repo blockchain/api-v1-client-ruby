c8319ff82e1b7314ae6b644659909dd0f09abf76
Peter Chudinov <peterchudinov@icloud.com>
2016-08-28 11:46:34 -0700

    make it pretty

--- lib/blockchain/createwallet.rb
+++ lib/blockchain/createwallet.rb
@@ -3,37 +3,37 @@ require_relative 'util'
 
 module Blockchain
 
-	def self.create_wallet(password, api_code, url, priv: nil, label: nil, email: nil)
+  def self.create_wallet(password, api_code, url, priv: nil, label: nil, email: nil)
 
-		params = { 'password' => password, 'api_code' => api_code }
-		
-		if !priv.nil?
-			params['priv'] = priv
-		end
-		if !label.nil?
-			params['label'] = label
-		end
-		if !email.nil?
-			params['email'] = email
-		end
-		
-		response = Blockchain::call_api('api/v2/create', method: 'post', data: params, base_url: url)
-		json_response = JSON.parse(response)
-		return CreateWalletResponse.new(json_response['guid'],
-										json_response['address'],
-										json_response['label'])
-	end
+    params = { 'password' => password, 'api_code' => api_code }
+    
+    if !priv.nil?
+      params['priv'] = priv
+    end
+    if !label.nil?
+      params['label'] = label
+    end
+    if !email.nil?
+      params['email'] = email
+    end
+    
+    response = Blockchain::call_api('api/v2/create', method: 'post', data: params, base_url: url)
+    json_response = JSON.parse(response)
+    return CreateWalletResponse.new(json_response['guid'],
+                    json_response['address'],
+                    json_response['label'])
+  end
 
-	class CreateWalletResponse
-		attr_reader :identifier
-		attr_reader :address
-		attr_reader :label
+  class CreateWalletResponse
+    attr_reader :identifier
+    attr_reader :address
+    attr_reader :label
 
-		def initialize(identifier, address, label)
-			@identifier = identifier
-			@address = address
-			@label = label
-		end
-	end
+    def initialize(identifier, address, label)
+      @identifier = identifier
+      @address = address
+      @label = label
+    end
+  end
 
 end
