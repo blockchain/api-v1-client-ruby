774fe0ddb3f302b65d3bb8cc11b679076a3320fa
Assaf <assafyossifoff@sky.com>
2017-04-20 17:37:08 +0100

    upgraded pushtx

--- lib/blockchain/pushtx.rb
+++ lib/blockchain/pushtx.rb
@@ -1,16 +1,29 @@
 require 'json'
-require_relative 'util'
+require_relative 'client'
 
 module Blockchain
 
-	def self.pushtx(tx, api_code = nil)
-		params = { 'tx' => tx }
-		
-		if !api_code.nil?
-			params['api_code'] = api_code
-		end
-		
-		Blockchain::call_api('pushtx', method: 'post', data: params)
-	end
+    class PushTx
 
+        attr_reader :client
+
+        def initialize(base_url = nil, api_code = nil)
+            @client = Client.new(base_url, api_code)
+        end
+
+        def pushtx(tx)
+            params = { 'tx' => tx }
+            @client.call_api('pushtx', method: 'post', data: params)
+        end
+    end
+
+    private
+    def self.proxy(method_name, api_code, tx)
+        warn "[DEPRECATED] avoid use of static methods, use an instance of PushTx class instead."
+        PushTx.new(nil, api_code).send(method_name, tx)
+    end
+
+	def self.pushtx(tx, api_code = nil)
+        self.proxy(__method__, api_code, tx)
+	end
 end
